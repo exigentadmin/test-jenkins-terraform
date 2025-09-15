fd8648755d2ad6492e51fd5d08cedeeabf362273
Dale Murdock <oceanviewllc@outlook.com>
2025-09-14 22:51:51 -0400

    Test deploy to AWS7

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ output "message" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-unique-bucket-name-{random_integer.this.result}"
-    tags = {
-      Name        = "My bucket"
-      Environment = "Dev"
-    }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 
 }
 
 resource "random_integer" "this" {
-    min = 373959309
-    max = 999999999
+  min = 373959309
+  max = 999999999
 }
